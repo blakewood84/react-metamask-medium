ef4c868597d4a2d93fc3fcf4e62395d47e749de6
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-11-06 19:12:05 -0700

    added useMetaMask hook

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="secondary">
+        <Button variant="secondary" onClick={connect}>
           <img src="images/metamask.svg" alt="MetaMask" width="50" height="50" /> Connect to MetaMask
         </Button>
         <div className="mt-2 mb-2">
-          Connected Account:
+          Connected Account: { isActive ? account : '' }
         </div>
-        <Button variant="danger">
+        <Button variant="danger" onClick={disconnect}>
           Disconnect MetaMask<img src="images/noun_waving_3666509.svg" width="50" height="50" />
         </Button>
       </header>
